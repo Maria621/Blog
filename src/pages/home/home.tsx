cd04b45b6aa5f92ca72a7386f32b984b2c4bb221
Maria De Fátima <133269366+Maria621@users.noreply.github.com>
2023-08-10 12:38:07 -0300

    atualizando

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,6 +1,6 @@
 
 import homeLogo from '../../assets/home.png'
-import './Home.css';
+
 import ListaPostagens from '../../components/postagens/listaPostagens/ListaPostagens';
 import ModalPostagem from '../../components/postagens/modelPostagens/ModelPostagem';
 
